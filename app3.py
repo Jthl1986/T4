9c487d584407d53aef83402486254ee9e6b3f449
Santacecilia <jorgesantacecilia@gmail.com>
2023-04-29 19:09:48 -0300

    9

--- app3.py
+++ app3.py
@@ -506,6 +506,15 @@ def app5():
         fig1 = px.pie(names=df_agrupado.index, values=df_agrupado.values, 
                      labels={'names':'Tipo de campo', 'values':'Superficie (has)'}) 
         
+        st.markdown("""
+        <style>
+            .piechart .g.pielayer text {
+                font-size: 12px !important;
+                transform: translate(30px, -5px) !important;
+            }
+        </style>
+        """, unsafe_allow_html=True)
+        
         # Mostrar el gráfico en la aplicación con Streamlit
         middle.plotly_chart(fig1)
         
