982fa3f66119dfd862571062c0f4a40dc7793105
Santacecilia <jorgesantacecilia@gmail.com>
2023-04-28 22:24:19 -0300

    9

--- app3.py
+++ app3.py
@@ -515,7 +515,7 @@ def app5():
                        font=dict(family='sans-serif',  # Cambiar la fuente a sans-serif
                                  size=14),
                        align='left',
-                       height=20))
+                       height=30))
         ])
         
         # Mostrar la tabla en la aplicación con Streamlit
